35e6eb093e0492434b8cbd6e2ae4b9e73feb1a48
farhan <ff1917914@gmail.com>
2024-09-22 15:27:08 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,203 @@
+
+
+
+
+
+
+// chapter no 14 
+
+// Q1
+
+// var studentNames = [];
+
+// Q2
+
+// var students = {
+//     names: []
+// };
+
+// Q3
+
+// var stringArray = ["mango", "apple", "Cherry", "banana"];
+
+// Q4
+
+// var numbersArray = [10, 20, 30, 40, 50];
+
+// Q5
+
+// var bolean = [true, false, true, false];
+
+// Q6
+
+// var Array = [19, "hello", true, null, { name: "farhan" }, [1, 2, 3]];
+
+// Q7
+
+//  var edu = "edu";
+
+//  document.write(`
+//     <h1>Qualification;
+//     </h1>
+//     1) SSC <br>
+//     2) HSC <br>
+//     3) BSC <br>
+//     4) BS <br>
+//     5) BCOM <br>
+//     6) MS <br>
+//     7) M.Phil. <br>
+//     8) PhD <br>
+    
+    
+    
+//     `)
+
+
+// Q8
+
+
+// document.write(`
+    
+//     <h1> Score is Micheal is 320: Percentage 64%</h1>
+    // 
+//     Score of Jhon is 230: Percentage 46%<br>
+//     Score of Tony is 480: Percentage 96%
+
+    
+//     `)
+
+
+// Q10
+
+
+// document.write(`
+    
+//     <h1> Score of students is: 320,230,480,120:<br>
+//     Orederd Scores of students : 120,230,320,480:
+
+//     </h1>
+    
+    
+    
+//     `)
+
+// Q1
+
+
+// var studentNames = [];
+
+// Q2
+
+
+// var stringArray = ["mango", "apple", "Cherry", "banana"];
+
+
+// Q3
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); 
+
+// Q4
+
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var totalLength = alphabet.length;
+// console.log(totalLength); 
+
+
+// Q5
+
+
+// var Array = ["Muhammad"];
+
+
+// Array.push("Faraz");
+
+
+// console.log(Array); 
+
+
+
+// Q6
+
+
+// var Arr = ["Hello"];
+
+
+// console.log(Arr); 
+
+
+// Q7
+
+// var Alphabet = ["h", "i", "j", "k"];
+// console.log(Alphabet); 
+
+// Q8
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(5); 
+// console.log(Alphabet);
+
+// Q9
+
+// var word = ["S", "M", "XL", "XXL", "XXXL"];
+// word.shift();
+// console.log(word); 
+
+
+
+// Q10
+
+// var word = ["S", "M", "XL", "XXL", "XXXL"];
+// word.unshift(1, 2, 3); 
+// console.log(word);
+
+
+// Q11
+
+// var word = ["S", "M", "XL", "XXL", "XXXL"];
+// word.unshift(1, 2, 3);
+// console.log(word);
+
+
+// Q12
+
+
+// var Arr = ["farooq"];
+// Arr.unshift("sheikh"); 
+// alert(Arr[0]); 
+
+
+// Q13
+
+
+// var words = ["S", "M", "XL", "XXL", "XXXL"];
+// words.splice(2, 0, "L");
+// console.log(words); 
+
+// Q14
+
+
+// var wrds = ["S", "M", "XL", "XXL", "XXXL"];
+// var   wrds(0, 3);
+// console.log(wrds); 
+
+// Q15
+
+
+// var fruits = ["apple", "mango", "banana", "grapes", "orange"];
+// fruits.splice(1, 3); 
+// fruits.splice(1, 0, "mango", "apple"); 
+// console.log(fruits); 
+
+// Q16
+
+// var fruits = ["apple", "mango", "banana", "grapes", "orange"];
+// fruits.splice(1, 2); 
+// console.log(fruits); 
+
+// Q17
+
+// var fruits = ["apple", "mango", "banana", "grapes", "orange","pineapple"];
+// var fruits.slice(3, 5); 
+// console.log(fruits); 
